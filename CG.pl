f349e439bf876ff5d5f81ffb74d5e477cbbf0114
mahmednabil109 <mahmednabil109@gmail.com>
2022-12-24 05:08:37 +0200

    follow required signature
    Signed-off-by: mahmednabil109 <mahmednabil109@gmail.com>

--- CG.pl
+++ CG.pl
@@ -5,21 +5,20 @@ coast_guard(X, Y, 0, REMAINING_SHIPS, s0):-
 	agent_loc(X, Y),
 	ships_loc(REMAINING_SHIPS).
 
-coast_guard(X, Y, OB, RS, S):-
-	coast_guard(X1, Y1, OB1, RS1, S1),
+coast_guard(X, Y, OB, RS, result(AC, S)):-
+	coast_guard(X1, Y1, OB1, RS1, S),
 	(
-		(X is X1 - 1, X >= 0, Y1 = Y, OB1 = OB, RS1 = RS, S = result(up, S1));
-		(X is X1 + 1, grid(M, _), X < M , Y1 = Y, OB1 = OB, RS1 = RS, S = result(down, S1));
-		(Y is Y1 - 1, Y >= 0, X1 = X, OB1 = OB, RS1 = RS, S = result(left, S1));
-		(Y is Y1 + 1, grid(_, N), Y < N , X1 = X, OB1 = OB, RS1 = RS, S = result(right, S1));
-		(member([X1, Y1], RS1), delete(RS1, [X1, Y1], RS), X = X1, Y = Y1, OB is OB1 + 1, capacity(C), C >= OB, S = result(pickup, S1));
-		(station(X1, Y1), OB1 > 0, OB = 0, X = X1, Y = Y1, RS = RS1, S = result(drop, S1))
+		(X is X1 - 1, X >= 0, Y1 = Y, OB1 = OB, RS1 = RS, AC = up);
+		(X is X1 + 1, grid(M, _), X < M , Y1 = Y, OB1 = OB, RS1 = RS, AC = down );
+		(Y is Y1 - 1, Y >= 0, X1 = X, OB1 = OB, RS1 = RS, AC = left );
+		(Y is Y1 + 1, grid(_, N), Y < N , X1 = X, OB1 = OB, RS1 = RS, AC = right);
+		(member([X1, Y1], RS1), delete(RS1, [X1, Y1], RS), X = X1, Y = Y1, OB is OB1 + 1, capacity(C), C >= OB, AC = pickup);
+		(station(X1, Y1), OB1 > 0, OB = 0, X = X1, Y = Y1, RS = RS1, AC = drop)
 	).
 
 
 % Goal State
 goal(S):-
-	S  = result(drop, _),
 	coast_guard(X, Y, 0, [], S),
 	station(X, Y).
 
